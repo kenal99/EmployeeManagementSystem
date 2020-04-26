9b6f268db4ffdaedec20fcd45ad69b7a63b55de1
kb99 <36529448+kenal99@users.noreply.github.com>
2020-04-26 19:05:53 +0530

    Update EmployeeLeaveRequestRepository.java

--- EmployeeMgmt/src/main/java/com/example/EmployeeMgmt/repository/EmployeeLeaveRequestRepository.java
+++ EmployeeMgmt/src/main/java/com/example/EmployeeMgmt/repository/EmployeeLeaveRequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.EmployeeMgmt.model.EmployeeLeaveRequest;
 import com.example.EmployeeMgmt.model.EmployeeLeaves;
 
-public interface EmployeeLeaveRequestRepository extends MongoRepository<EmployeeLeaveRequest, String> {
+public interface EmployeeLeaveRequestRepository extends MongoRepository<EmployeeLeaveRequest,String> {
 
+	
 }
